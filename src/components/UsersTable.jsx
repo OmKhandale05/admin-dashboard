aadad4b3950bfa8b4213905f768d4bed3b3870b9
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-09 20:12:21 +0530

    Feature: add user search functionality

--- src/components/UsersTable.jsx
+++ src/components/UsersTable.jsx
@@ -1,11 +1,25 @@
 import "../styles/UsersTable.css";
 import { users } from "../data/users";
-
+import { useState } from "react";
 
 const UsersTable = () => {
+  const [search, setSearch] = useState("");
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="table-container">
       <h3 className="table-title">Recent Users</h3>
+
+      <input
+        type="text"
+        placeholder="Search users .."
+        className="search-input"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
 
       <table className="users-table">
         <thead>
@@ -17,7 +31,7 @@ const UsersTable = () => {
           </tr>
         </thead>
         <tbody>
-          {users.map((user) => (
+          {filteredUsers.map((user) => (
             <tr key={user.id}>
               <td>{user.id}</td>
               <td>{user.name}</td>
