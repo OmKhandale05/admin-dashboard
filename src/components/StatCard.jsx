047bd5fa8a5791069d690bcd5ba63d364b4d5c75
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-09 17:11:41 +0530

    Feature: Create stat card component

---
+++ src/components/StatCard.jsx
@@ -0,0 +1,10 @@
+const StatCard = ({title, value}) => {
+    return ( 
+        <div className="stat-card">
+            <h4 className="stat-title">{title}</h4>
+            <p className="stat-value">{value}</p>
+        </div>
+     );
+}
+ 
+export default StatCard;
