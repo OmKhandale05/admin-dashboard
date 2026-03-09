3a1ef9bc6802fbcca3b82e8bebf8164352a6b719
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-10 02:33:20 +0530

    Feature: add add-product functionality in producs page

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,17 +1,24 @@
 import { useState } from "react";
-import{ products as initialProducts } from "../data/products";
+import { products as initialProducts } from "../data/products";
 
 const useProducts = () => {
-    const [productList, setProductList] = useState(initialProducts);
+  const [productList, setProductList] = useState(initialProducts);
 
-    const deleteProduct = (id) =>{
-        setProductList(productList.filter(product => product.id !== id));
-    };
-    return {
-        productList,
-        deleteProduct
-    };
-    
+  const deleteProduct = (id) => {
+    setProductList(productList.filter((product) => product.id !== id));
+  };
+
+  const addProduct = (product) => {
+    setProductList([
+      ...productList,
+      { id: productList.length + 1, ...product },
+    ]);
+  };
+  return {
+    productList,
+    deleteProduct,
+    addProduct
+  };
 };
- 
+
 export default useProducts;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,12 @@
 import Table from "../components/Table";
 import useProducts from "../hooks/useProducts";
 import "../styles/Products.css";
+import { useState } from "react";
+import Modal from "../components/Modal";
 
 const Products = () => {
-  const { productList, deleteProduct } = useProducts();
+  const { productList, deleteProduct, addProduct } = useProducts();
+  const [isOpen, setIsOpen] = useState(false);
 
   const columns = [
     { header: "ID", accessor: "id" },
@@ -12,9 +15,41 @@ const Products = () => {
     { header: "Stock", accessor: "stock" },
   ];
 
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    stock: ""
+  });
+  const handleChange = (e) => {
+    setNewProduct({
+      ...newProduct,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const saveProduct = () => {
+    addProduct(newProduct);
+
+    setNewProduct({
+      name: "",
+      price: "",
+      stock: ""
+    });
+
+    setIsOpen(false);
+  };
+
+
   return (
     <div className="products-page">
       <h1>Products</h1>
+
+      <button
+          className="add-product-btn"
+          onClick={() => setIsOpen(true)}
+        >
+          Add Product
+        </button>
 
       <Table
         columns={columns}
@@ -28,9 +63,43 @@ const Products = () => {
             >
               Delete
             </button>
+
           </div>
         )}
       />
+      <Modal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+
+        <h2>Add Product</h2>
+
+        <input
+          name="name"
+          placeholder="Product Name"
+          value={newProduct.name}
+          onChange={handleChange}
+        />
+
+        <input
+          name="price"
+          placeholder="Price"
+          value={newProduct.price}
+          onChange={handleChange}
+        />
+
+        <input
+          name="stock"
+          placeholder="Stock"
+          value={newProduct.stock}
+          onChange={handleChange}
+        />
+
+        <button onClick={saveProduct}>
+          Save Product
+        </button>
+
+      </Modal>
     </div>
   );
 };
