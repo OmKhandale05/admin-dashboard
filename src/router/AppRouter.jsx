ceaa8f74453542a0cddaeff19247c0b632e6487c
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-09 16:15:02 +0530

    Feature: Setup routing system

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,30 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import Dashboard from "../pages/Dashboard";
+import Orders from "../pages/Orders";
+import Products from "../pages/Products";
+import Settings from "../pages/Settings";
+import Users from "../pages/Users";
 
 const AppRouter = () => {
-    return ( 
-        <BrowserRouter></BrowserRouter>
-     );
-}
- 
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div>
+        <Sidebar />
+        <div>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
 export default AppRouter;
