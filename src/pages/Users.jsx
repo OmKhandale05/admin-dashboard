14c94237c555854b39bae06f28ce73078200040f
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-10 18:41:10 +0530

    Modify: add search and pagination to users table

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -3,9 +3,19 @@ import Table from "../components/Table";
 import { users } from "../data/users";
 import Modal from "../components/Modal";
 import "../styles/Users.css";
+import usePagination from "../hooks/usePagination";
 
 const Users = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+
+  const { currentItem, currentPage, totalPages, nextPage, prevPage } =
+    usePagination(filteredUsers, 5);
 
   const columns = [
     { header: "ID", accessor: "id" },
@@ -14,23 +24,44 @@ const Users = () => {
     { header: "Role", accessor: "role" },
   ];
   return (
-    <div>
-      <h1>Users</h1>
+    <div className="users-page">
+      <div className="users-header">
+        <h1>Users</h1>
 
-      <button className="add-btn" onClick={() => setIsOpen(true)}>
-        Add User
-      </button>
+        <input
+          type="text"
+          placeholder="Search users .."
+          className="search-input"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
 
-      <Table
-        columns={columns}
-        data={users}
-        actions={(user) => (
-          <div className="table-actions">
-            <button className="edit-btn">Edit</button>
-            <button className="delete-btn">Delete</button>
-          </div>
-        )}
-      />
+        <button className="add-btn" onClick={() => setIsOpen(true)}>
+          Add User
+        </button>
+      </div>
+
+      <div className="table-card">
+        <Table
+          columns={columns}
+          data={currentItem}
+          actions={(user) => (
+            <div className="table-actions">
+              <button className="edit-btn">Edit</button>
+              <button className="delete-btn">Delete</button>
+            </div>
+          )}
+        />
+      </div>
+
+      <div className="pagination">
+        <button onClick={prevPage}>Previous</button>
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+
+        <button onClick={nextPage}>Next</button>
+      </div>
 
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <h2>Add User</h2>
