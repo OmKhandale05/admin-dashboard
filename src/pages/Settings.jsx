753fa158b184d0436eae928334a18fd5e821c5ce
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-10 15:36:13 +0530

    Feature: add settings page and its css

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,9 +1,48 @@
+import { useState } from "react";
+import "../styles/Settings.css"
+
 const Settings = () => {
-    return ( 
-        <div>
-            <h1>Settings</h1>
+  const [formData, setFormData] = useState({
+    name: "Admin",
+    email: "admin@example.com",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const saveSettings = () => {
+    alert("sattings saved.");
+  };
+  return (
+    <div className="setting-page">
+      <h1>Settings</h1>
+      <div className="settings-card">
+        <div className="form-group">
+          <label>Name</label>
+          <input name="name" value={formData.name} onChange={handleChange} />
         </div>
-     );
-}
- 
+        <div className="form-group">
+          <label>Email</label>
+          <input name="email" value={formData.email} onChange={handleChange} />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button className="save-settings-btn" onClick={saveSettings}>Save Changes</button>
+      </div>
+    </div>
+  );
+};
+
 export default Settings;
