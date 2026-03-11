2139f37c87746cbaf6fcc19bdcb967d329aa8b98
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-11 20:12:13 +0530

    Feature: add dark and light mode functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
 import "../styles/Navbar.css"
 
+
 const Navbar = () => {
+
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || "light"
+  );
+
+  useEffect(() => {
+    document.body.className = theme;
+    localStorage.setItem("theme",theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
+
   return (
     <div className="navbar">
       <h3 className="navbar-title">Admin Dashboard</h3>
+
+      <button onClick={toggleTheme} className="theme-btn">
+        {theme === "light" ? "Dark" : "Light"}
+      </button>
 
       <div className="navbar-right">
         <div className="profile">Admin</div>
